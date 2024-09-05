2d82a3d52877e599900c52b9378878295df40eb1
s0if <saifalkomi@gmail.com>
2024-09-05 18:17:27 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for(int c = 1;c<10 ; c++) 
+            {
+               int number=int.Parse( Console.ReadLine());
+                list.Add(number);
+            }
+            list.Sort();
+            for (int c =0; c < 9; c++)
+            {
+                if (list[c]!=c+1)
+                {
+                    Console.WriteLine($"missing : {list[c] - 1}");
+                    break;
+                }
+            }
+        }
+    }
+}
